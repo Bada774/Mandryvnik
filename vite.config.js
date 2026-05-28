5549d8a6a1d15d6d69276f799516bd2bdbfef8de
Bada774 <vladislavy.karelin@gmail.com>
2026-05-28 15:56:37 +0300

    links fix

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,17 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 import handlebars from "vite-plugin-handlebars";
 
+const base = "/Mandryvnik/";
+
 export default defineConfig({
-    base: "/Mandryvnik/",
+    base,
     root: "src",
     publicDir: "../public",
 
     plugins: [
         handlebars({
             partialDirectory: resolve(__dirname, "src/partials"),
+            context: { base },
         }),
     ],
 
